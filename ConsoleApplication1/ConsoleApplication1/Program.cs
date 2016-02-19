3c9d038b4b758e441f24e599e396f573a861ee01
Stojkovski Vasko <stojkovski.vasko.vs@gmail.com>
2016-02-19 16:45:40 +0100

    C# Code
    C# Homework

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        //static int concatStrangeThings()
+
+        //{
+        //    int a = 6;
+        //    string b = "12";
+        //    int c = a + int.Parse(b);
+        //    Console.WriteLine(c);
+        //    Console.ReadLine();
+        //    return c;
+        //}
+        static int theApplesAndTheBaskets()
+        {
+            Console.WriteLine("Enter the number of apples on the tree");
+            int apples = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter how much apples can you put in one basket");
+            int basket = int.Parse(Console.ReadLine());
+            int baskets;
+            int tempBaskets = apples % basket;
+
+            if (tempBaskets == 0)
+
+            {
+
+                baskets = apples / basket;
+                Console.WriteLine(baskets);
+            }
+            
+            else if (tempBaskets >= 1) 
+            
+            {
+                baskets = apples / basket + 1;
+                Console.WriteLine(baskets);
+            }
+            
+            
+            Console.ReadLine();
+            return tempBaskets;
+        }
+
+        static void Main(string[] args)
+
+        {
+           Console.WriteLine(theApplesAndTheBaskets());
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static int smsMessages()
+        {
+            int messages;
+            Console.WriteLine("Enter the credits on your mobile bill");
+            float credits = float.Parse(Console.ReadLine());
+            Console.WriteLine("How much is the cost of one message");
+            float cost = float.Parse(Console.ReadLine());
+
+            float tempMessages = credits / cost;
+            messages = (int)tempMessages;
+            //if (tempMessages < cost)
+           // {
+                //int messagess = (int)tempMessages;
+                Console.WriteLine("You can write " + messages + " messages");
+                Console.ReadLine();
+            //}
+            return messages;  
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(smsMessages());
+            Console.ReadLine();
+        }
+    }
+}
