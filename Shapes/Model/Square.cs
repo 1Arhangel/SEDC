73fc00721390120b8c23c96ad97cf641bf159fbb
Stojkovski Vasko <stojkovski.vasko.vs@gmail.com>
2016-02-27 15:32:41 +0100

    C# OOP

---
+++ Shapes/Model/Square.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Square
+    {
+        public int Side { get; set; }
+
+        public int CalculateArea()
+        {
+            return this.Side * this.Side;
+        }
+
+        public int CalculatePerimeter()
+        {
+            return 4 * this.Side;
+        }
+
+    }
+}
---
+++ Shapes/Shapes/Program.cs
@@ -0,0 +1,25 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square = new Square();
+
+            Console.WriteLine("Enter the square side");
+            square.Side = int.Parse(Console.ReadLine());
+            
+
+            Console.WriteLine("The Area is " +square.CalculateArea());
+
+            Console.WriteLine("The Perimeter is "+square.CalculatePerimeter());
+        }
+    }
+}
