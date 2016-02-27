9a063b10490721809ad834e10be7402fd1e16af7
Stojkovski Vasko <stojkovski.vasko.vs@gmail.com>
2016-02-27 15:30:55 +0100

    C# OOP

---
+++ TestOOP/TestOOP/Program.cs
@@ -0,0 +1,25 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human vasko = new Human();
+            vasko.FirstName = "Vasko";
+            vasko.LastName = "Stojkovski";
+
+          
+
+             
+
+            Console.WriteLine(vasko.GetFullName());
+        }
+    }
+}
