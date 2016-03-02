83cc2e1dd536c77d61efb1b12454f92aeadc1e40
Stojkovski Vasko <stojkovski.vasko.vs@gmail.com>
2016-03-02 20:42:15 +0100

    Optional Parameters
    Constructor Params

---
+++ OptionalParameters/Human/Human.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Human
+{
+    public class Humans
+    {
+        
+        public Humans(string param1, string param2)
+        {
+            this.FirstName = param1;
+            this.LastName = param2;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        
+        public bool IsFullNameLongerThanMaxLength(int maxLength=0)
+        {
+            if (maxLength > this.FirstName.Length)
+
+                return true;
+            else
+                return false;
+        }
+
+        public string PrintWelcomeMessage(string welcomeMessage1="Pozdrav", string welcomeMessage2="Pozdrav pak")
+        {
+            return welcomeMessage1 + " " + this.FirstName + " " + this.LastName + " " +welcomeMessage2;
+        }
+
+
+    }
+}
---
+++ OptionalParameters/OptionalParameters/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Human;
+namespace OptionalParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Humans human = new Humans("Vasko", "Stojkovski");
+
+            
+
+
+            
+            
+            string welcomeMessage1 = "Welcome";
+            string welcomeMessage2 = "Welcome again";
+
+            Console.WriteLine(human.PrintWelcomeMessage("Hello","Hello again"));
+
+            Console.WriteLine(human.PrintWelcomeMessage(welcomeMessage1, welcomeMessage2));
+
+            Console.WriteLine(human.PrintWelcomeMessage());
+
+            Console.WriteLine(human.PrintWelcomeMessage("Zdravo"));
+
+            
+
+            Console.WriteLine(human.IsFullNameLongerThanMaxLength());
+
+            Console.ReadLine();
+        }
+    }
+}
