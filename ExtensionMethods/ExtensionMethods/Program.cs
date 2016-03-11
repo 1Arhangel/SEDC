21524c9263b2e52ad0a67168696c59117cf0b02c
Stojkovski Vasko <stojkovski.vasko.vs@gmail.com>
2016-03-11 20:24:38 +0100

    C# Extension Methods

---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+
+namespace ExtensionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = "Vasko";
+            Console.WriteLine(Name.WordCount());
+
+            StringExtensions.WordCount("432");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ExtensionMethods/Model/ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public static class StringExtensions
+    {
+        public static int WordCount(this string str)
+        {
+            return str.Length;
+        }
+    }
+}
