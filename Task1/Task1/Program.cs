71ce02634d7df247d920a13546a4211fc05201cd
Stojkovski Vasko <stojkovski.vasko.vs@gmail.com>
2016-03-15 16:43:10 +0100

    ArrayList
    AdvancedHomework1

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList hey = new ArrayList { 1, 2, 3.14, 4.4, "Vasko", "Stojkovski", };
+
+            foreach (var item in hey)
+            {
+                Console.WriteLine("The item "+item+" is of type "+item.GetType());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
