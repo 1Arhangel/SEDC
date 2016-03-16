2d9afadfad33b6d307aabc0422a0aa9c5274b4af
Stojkovski Vasko <stojkovski.vasko.vs@gmail.com>
2016-03-16 20:05:27 +0100

    C# Anonymous Types

---
+++ AnonymousTypes/AnonymousTypes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var teacher = new { Name = "Orce", Surname = "Petreski", Years = 38 };
+            
+            Console.WriteLine(teacher.GetType());
+            Console.ReadLine();
+
+            var anon = new [] { new { Name = "Orce"}, new {Name = "Vasko" } };
+        }
+    }
+}
